b8e3124a8ce8df42bb42e55bf36374b12f874a9f
akshaypandey28 <akshaychaman2004@gmail.com>
2026-04-01 00:32:02 +0530

    created Project Service and Fetching the Tree of Project Id

--- backend/src/controllers/projectController.js
+++ backend/src/controllers/projectController.js
@@ -1,27 +1,21 @@
-import util from 'util';
-import child_process, { exec } from 'child_process';
-import fs from 'fs/promises';
-import uuid4 from 'uuid4';
-import { REACT_PROJECT_COMMAND } from '../config/serverConfig.js';
-
-const execPromisified = util.promisify(child_process.exec);
+import { createProjectService, getProjectTreeService } from "../service/projectService.js"
 
 export const createProjectController = async (req,res) => {
-    //create a unique id and then inside the projects folder create a new folder with that id
-    const projectId = uuid4();
-    console.log(projectId);
-
-    await fs.mkdir(`./projects/${projectId}`);
     
-    //after this call the npm create vite command in the newly created project folder
-
-    const response = await execPromisified(REACT_PROJECT_COMMAND , {
-        cwd : `./projects/${projectId}`
-    });
-
+    const projectId = await createProjectService();
     
     return res.json({
-        message : 'Project created successfully',
+        message : 'Project created successfully via backend',
         data :projectId
+    })
+}
+
+export const getProjectTree = async(req,res) => {
+    const tree = await getProjectTreeService(req.params.projectId);
+
+    return res.status(200).json({
+        data:tree,
+        success:true,
+        message:'Project tree fetched successfully'
     })
 }
