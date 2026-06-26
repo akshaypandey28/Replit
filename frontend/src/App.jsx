e40b192705d9117dd33b0f650b559e2e3352ab02
akshaypandey28 <akshaychaman2004@gmail.com>
2026-06-26 10:40:16 +0530

    addtion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import { Router } from './Router.jsx'
+import {io} from 'socket.io-client'
 function App() {
-
+  const socket = io("http://localhost:3000")
   return (
     <Router/>
   )
